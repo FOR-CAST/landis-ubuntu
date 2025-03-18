48433805086d55ad66198eab203198403c1aced8
Alex Chubaty <achubaty@for-cast.ca>
2025-03-17 20:09:15 -0600

    docker: don't need ssh keys (no private repos)

--- docker/build_v7.sh
+++ docker/build_v7.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_ed25519_GitHub
-
-docker build --ssh default . -f docker/LANDIS-v7/Dockerfile -t achubaty/landis-ii-v7:latest
+docker build . -f docker/LANDIS-v7/Dockerfile -t achubaty/landis-ii-v7:latest
--- docker/build_v8.sh
+++ docker/build_v8.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_ed25519_GitHub
-
-docker build --ssh default . -f docker/LANDIS-v8/Dockerfile -t achubaty/landis-ii-v8:latest
+docker build . -f docker/LANDIS-v8/Dockerfile -t achubaty/landis-ii-v8:latest
